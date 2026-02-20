0b936f207f75c1f34a718c636ec03d5aaa817a00
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-02-20 15:03:05 +0100

    build: improve structure

--- App/app.py
+++ App/app.py
@@ -1,14 +1,6 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-# Disable static file cache during development so the browser doesn't get 304 Not Modified
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
+import flask
+from App import app
 
 @app.route('/')
 def hello_world():
-
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run()
+    return flask.render_template('index.html')
