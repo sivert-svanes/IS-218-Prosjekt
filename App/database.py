768cf3b173470e5efe63d65a985d0972e3f2de08
sivert-svanes <siverss@uia.no>
2026-02-22 13:15:07 +0100

    created database maneging file

---
+++ App/database.py
@@ -0,0 +1,12 @@
+﻿import sqlalchemy
+from sqlalchemy import text
+
+def create_connection():
+    engine = sqlalchemy.create_engine("")
+    connection = engine.connect()
+    return connection
+
+def database_test_query(connection):
+    with connection as conn:
+        result = conn.execute(text("SELECT * FROM pg_catalog.pg_tables;"))
+        print(result.all())
