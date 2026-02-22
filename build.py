547d5ad676e5512a31034a49b04d77a54e2c4263
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-02-22 15:10:04 +0100

    fix: fix fucked script

--- build.py
+++ build.py
@@ -2,6 +2,7 @@
 import platform
 import subprocess
 
+os.system("py -3 -m venv .venv")
 os.system("pip install flask")
 os.system("pip install sqlalchemy")
 os.system("pip install GeoAlchemy2")
@@ -11,15 +12,15 @@ os.system("pip install python-dotenv")
 os.system("pip install spglib")
 
 if platform.system() == "Windows":
-    subprocess.run(["powershell", r'cd .\frontend\; npm install; npm run build '])
+    subprocess.run(["powershell", r'cd .\App\frontend\; npm install; npm run build '])
 else:
     #not tested outside windows, so IDK if this works
-    subprocess.run(["cd frontend/; npm install; npm run build "])
+    subprocess.run(["cd App/frontend/; npm install; npm run build "])
 
 print("Enter db connection string:")
 conn_str = input()
 
-env = open(".env", "w")
+env = open("App/.env", "w")
 env.write("DATABASE_URL=" + conn_str)
 env.close()
 
@@ -28,7 +29,7 @@ start = input()
 
 if start == "y":
     if platform.system() == "Windows":
-        subprocess.run(["powershell", r' flask --app app.py run --debug'])
+        subprocess.run(["powershell", r'.venv\Scripts\activate; flask --app .\App\app.py run --debug'])
     else:
         #not tested outside windows, so IDK if this works
-        subprocess.run([r'flask --app app.py run --debug'])
+        subprocess.run([r'.venv/Scripts/activate; flask --app /App/app.py run --debug'])
