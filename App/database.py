63664223db2423373a1fcce4dd68a7cf7b926dc7
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-02-22 13:45:35 +0100

    db: load connstring

--- App/database.py
+++ App/database.py
@@ -1,9 +1,14 @@
 ﻿import sqlalchemy
 from sqlalchemy import text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+connStr = os.getenv("DATABASE_URL")
 
 def create_connection():
     #Put inn the env variable in the create engine function
-    engine = sqlalchemy.create_engine("")
+    engine = sqlalchemy.create_engine(connStr)
     connection = engine.connect()
     return connection
 
