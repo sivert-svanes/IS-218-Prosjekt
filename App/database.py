02b09f180eae3f7a5b54722f14cae9cc4e6c7895
sivert-svanes <siverss@uia.no>
2026-02-22 14:00:03 +0100

    created brannstasjoner model

--- App/database.py
+++ App/database.py
@@ -1,7 +1,10 @@
 ﻿import sqlalchemy
 from sqlalchemy import text
 from dotenv import load_dotenv
+from geoalchemy2 import Geometry
+from sqlalchemy import Column, Integer, String
 import os
+
 
 load_dotenv()
 connStr = os.getenv("DATABASE_URL")
@@ -16,3 +19,13 @@ def database_test_query(connection):
     with connection as conn:
         result = conn.execute(text("SELECT * FROM pg_catalog.pg_tables;"))
         print(result.all())
+
+class BrannstasjonerModel():
+    id = Column(Integer, primary_key=True)
+    geom = Column(Geometry(geometry_type='POINT', srid=4326))
+    gml_id = Column(String)
+    opphav = Column(String)
+    brannstasjon = Column(String)
+    brannvesen = Column(String)
+    stasjonstype = Column(String)
+    kasernert = Column(String)
